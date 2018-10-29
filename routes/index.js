b04e8814c9b1a314a3890d36fc8b78e4605350c5
Huy Tran <huytran@enZos-MacBook-Pro.local>
2018-10-29 11:50:05 +0700

    add show

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('indexHotel', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('hotelPage', { title: 'GALINA HOTEL' });
 });
 
-router.get('/:clang', function(req, res, next) {
-  console.log(req.param("_______: "+clang));
-  res.render('index', { title: 'Express' });
+router.get('/hotel', function (req, res) {
+  res.redirect('/');
 });
+
+router.get('/mudbathspa', function (req, res) {
+  res.render('mudspaPage', { title: 'GALINA MUD BATH & SPA' });
+});
+
+router.get('/weddingconvention', function (req, res) {
+  res.render('wcPage', { title: 'GALINA WEDDING CONVENTION' });
+});
+
+router.get('/show', function (req, res) {
+  res.render('showPage', { title: 'GALINA SHOW' });
+})
+
+router.get('/lakeview', function (req, res) {
+  res.render('lakePage', { title: 'GALINA LAKE VIEW' });
+})
+
+router.get('/water', function (req, res) {
+  res.render('waterPage', { title: 'GALINA LAKE VIEW' });
+})
 
 module.exports = router;
