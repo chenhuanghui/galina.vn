a9b8da6109afe5a31c08b99b09d260a89adb4120
Huy Tran <huytran@enZos-MBP.lan>
2018-11-10 17:45:43 +0700

    fix dynamic data

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,62 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('hotelPage', { title: 'GALINA HOTEL' });
+  let sliders = ['img/hotel/slider01.jpg','img/hotel/slider02.jpg','img/hotel/slider03.jpg','img/hotel/slider04.jpg','img/hotel/slider05.jpg'];
+  let sliders_thumb= ['img/hotel/slider01_thumbnail.jpg','img/hotel/slider02_thumbnail.jpg','img/hotel/slider03_thumbnail.jpg','img/hotel/slider04_thumbnail.jpg','img/hotel/slider05_thumbnail.jpg'];
+  let gallery = ['img/gallery/gallery01.jpg','img/gallery/gallery02.jpg','img/gallery/gallery03.jpg','img/gallery/gallery04.jpg','img/gallery/gallery05.jpg','img/gallery/gallery06.jpg','img/gallery/gallery07.jpg','img/gallery/gallery08.jpg','img/gallery/gallery09.jpg','img/gallery/gallery10.jpg','img/gallery/gallery11.jpg','img/gallery/gallery12.jpg'];
+  let roomlist = [
+    {
+      name:'Supperior',
+      link: '#',
+      imgRoom:'img/hotel/room-superior.jpg',
+      square: '32',
+      windows:'yes',
+      balcone:'no',
+      view:'city',
+      capacity:'02 adults, 02 kid',
+      price: '1.800.000'
+    },
+    {
+      name:'Deluxe',
+      link: '#',
+      imgRoom:'img/hotel/room-superior.jpg',
+      square: '32',
+      windows:'yes',
+      balcone:'no',
+      view:'city',
+      capacity:'02 adults, 02 kid',
+      price: '1.800.000'
+    },
+    {
+      name:'Suite City',
+      link: '#',
+      imgRoom:'img/hotel/room-superior.jpg',
+      square: '32',
+      windows:'yes',
+      balcone:'no',
+      view:'city',
+      capacity:'02 adults, 02 kid',
+      price: '1.800.000'
+    },
+    {
+      name:'Suite View',
+      link: '#',
+      imgRoom:'img/hotel/room-superior.jpg',
+      square: '32',
+      windows:'yes',
+      balcone:'no',
+      view:'city',
+      capacity:'02 adults, 02 kid',
+      price: '1.800.000'
+    }
+  ]
+  res.render('hotelPage', { 
+    title: 'GALINA HOTEL',
+    sliders: sliders,
+    sliders_thumb: sliders_thumb,
+    gallery: gallery,
+    roomlist: roomlist
+  });
 });
 
 router.get('/hotel', function (req, res) {
