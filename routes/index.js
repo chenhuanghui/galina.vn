6ffae4695ba3712da0b6def50ba5dc38e8b94dd0
Huy Tran <huytran@enZos-MacBook-Pro.local>
2018-11-14 21:59:52 +0700

    add nodemailer

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var nodemailer =  require('nodemailer'); // khai báo sử dụng module nodemailer
 
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   let sliders = ['img/hotel/slider01.jpg','img/hotel/slider02.jpg','img/hotel/slider03.jpg','img/hotel/slider04.jpg','img/hotel/slider05.jpg'];
