494df50e565333de9a39bd41c870eeb69e398163
Huy Tran <huytran@enZos-MBP.lan>
2018-11-16 14:48:25 +0700

    fix sendgrid_key

--- routes/index.js
+++ routes/index.js
@@ -92,7 +92,6 @@ router.get('/water', function (req, res) {
 
 
 router.post('/sendmail', function(req, res){
-  
   var bodyEmail = ' • Checkin: ' + req.body.ci 
                   + '\n • Check out: ' + req.body.co 
                   + '\n • Adult: ' + req.body.adult 
@@ -100,28 +99,27 @@ router.post('/sendmail', function(req, res){
                   + '\n • Tel:' + req.body.tel 
                   + '\n • Name: ' + req.body.name 
                   + '\n • Room name: ' + req.body.room;
-  res.send(bodyEmail);
-  // res.send (bodyEmail);
-  // var fromEmail = new helper.Email('no-reply@haidanggroup.com');
-  // var toEmail = new helper.Email(req.data.email);
-  // var subject = 'This is email confirmed booking' + req.data.ci + '-' + req.data.co;
-  // var content = new helper.Content('text/plain', bodyEmail);
 
-  // var mail = new helper.Mail(fromEmail, subject, toEmail, content);
-  // var sg = require('sendgrid')('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');
+  var fromEmail = new helper.Email('no-reply@haidanggroup.com');
+  var toEmail = new helper.Email(req.data.email);
+  var subject = 'This is email confirmed booking' + req.data.ci + '-' + req.data.co;
+  var content = new helper.Content('text/plain', bodyEmail);
 
-  // var request = sg.emptyRequest({
-  //   method: 'POST',
-  //   path: '/v3/mail/send',
-  //   body: mail.toJSON()
-  // });
+  var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+  var sg = require('sendgrid')(process.env.SENDGRID_KEY);
+
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
 
 
-  // sg.API(request, function (error, response) {
-  //   if (error) {
-  //     return 0
-  //   }
-  //   return response.statusCode;
-  // });
+  sg.API(request, function (error, response) {
+    if (error) {
+      res.send(0);
+    }
+    res.send(statusCode);
+  });
 })
 module.exports = router;
