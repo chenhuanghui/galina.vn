717fda7cec6db5468b0024120dbced705c26be6c
Huy Tran <huytran@enZos-MBP.lan>
2018-11-19 18:55:10 +0700

    fix booking page

--- routes/index.js
+++ routes/index.js
@@ -90,13 +90,23 @@ router.get('/water', function (req, res) {
 
 
 router.post('/sendmail', function(req, res){
-  var bodyEmail = ' • Checkin: ' + req.body.ci 
+  var bodyEmail = 'Dear value Customer, \n GALINA HOTEL already got your reservation, this is an email auto generate from our booking engine to confirmed your booking information. \nOur reservation team will handle your booking and notify you ASAP.'
+                  + '\n Your booking information is below: '
+                  + '\n • Checkin: ' + req.body.ci 
                   + '\n • Check out: ' + req.body.co 
                   + '\n • Adult: ' + req.body.adult 
                   + '\n • Kid: ' + req.body.kid 
                   + '\n • Tel:' + req.body.tel 
                   + '\n • Name: ' + req.body.name 
-                  + '\n • Room name: ' + req.body.room;
+                  + '\n • Room name: ' + req.body.room
+                  + '\n\n Thanks & Best regard,'
+                  + '\n GALINA RESERVATION TEAM'
+                  + '\n ____________________________'
+                  + '\n • email: booking@haidanggroup.com | hotline: (+84) 901 700089 | '
+                  + '\n • website: www.galina.vn | addr: 05 Hung Vuong, Nha Trang City, Khanh Hoa, Viet Nam'
+                  + '\n This communication may contain information that is proprietary, confidential, or exempt from disclosure. '
+                  + '\n If you are not the intended recipient, please note that any other dissemination, distribution, use or copying of this communication is strictly prohibited. '
+                  + '\n Anyone who receives this message in error should notify the sender immediately by telephone or by return e-mail and delete it from his or her computer.';
 
   var mailTemp = req.body.email + ',booking@haidanggroup.com,reservation.galina@haidanggroup.com,gsales@haidanggroup.com';
   // res.send(mailTemp);
