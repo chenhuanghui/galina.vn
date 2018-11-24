2c9941e25eaca1372c1121c5d37bf696d3080648
Huy Tran <huytran@enZos-MBP.lan>
2018-11-24 20:03:11 +0700

    add facebook chat

--- public/js/core.js
+++ public/js/core.js
@@ -27,7 +27,7 @@ var Core = {
             this.datepicker();
             this.masonry();
             this.language();
-            // this.messengerSupport();
+            this.messengerSupport();
         },
         language: function(){
             if ($.cookie('clang') === 'undefined') {
@@ -49,22 +49,8 @@ var Core = {
             });
         },
         messengerSupport: function(){
-            window.fbAsyncInit = function() {
-                FB.init({
-                    appId            : '265524687478457',
-                    autoLogAppEvents : true,
-                    xfbml            : true,
-                    version          : 'v3.2'
-                });
-                };
-            
-                (function(d, s, id){
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) {return;}
-                    js = d.createElement(s); js.id = id;
-                    js.src = "https://connect.facebook.net/en_US/sdk.js";
-                    fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));
+            console.log('init facebook application')
+
         },
         animations: function() {
             // Animation - appear 
