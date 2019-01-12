f1abf51ca6329bf14bbec9987478d9097c0138b1
Huy Tran <inbox.huytran@gmail.com>
2019-01-12 13:17:51 +0700

    add theme brown & remove trash css

--- routes/index.js
+++ routes/index.js
@@ -51,8 +51,14 @@ router.get('/show', function (req, res) {
 })
 
 router.get('/lakeview', function (req, res) {
+  let sliders = ['img/show/slider01.jpg','img/show/slider02.jpg','img/show/slider03.jpg','img/show/slider04.jpg','img/show/slider05.jpg'];
+  let sliders_thumb= ['img/show/slider01_thumbnail.jpg','img/show/slider02_thumbnail.jpg','img/show/slider03_thumbnail.jpg','img/show/slider04_thumbnail.jpg','img/show/slider05_thumbnail.jpg'];
+  let gallery = ['img/show/gallery/gallery01.jpg','img/show/gallery/gallery02.jpg','img/show/gallery/gallery03.jpg','img/show/gallery/gallery04.jpg','img/show/gallery/gallery05.jpg','img/show/gallery/gallery06.jpg','img/show/gallery/gallery07.jpg','img/show/gallery/gallery08.jpg','img/show/gallery/gallery09.jpg','img/show/gallery/gallery10.jpg','img/show/gallery/gallery11.jpg','img/show/gallery/gallery12.jpg'];
   res.render('lakePage', { 
     title: 'GALINA LAKE VIEW',
+    sliders: sliders,
+    sliders_thumb: sliders_thumb,
+    gallery: gallery,
     pageID: 5, 
     ref: 'lakeview'
   });
